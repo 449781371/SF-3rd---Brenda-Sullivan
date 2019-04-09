7eae2a8b12ef900f46e0ee323d724f4108c32e27
449781371 <47670285+449781371@users.noreply.github.com>
2019-04-09 10:36:57 -0500

    Add files via upload

---
+++ JaredsMainActivity.java
@@ -0,0 +1,17 @@
+package org.pltw.example.sfduedate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //This method would create and constrain groups when the button is clocked
+    //public void onFloatingActionButtonClick{View view}{
+        //group.create
+    //}
+}
