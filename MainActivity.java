0c99815dc8bb003a4adbba5de41685917e1ee51c
589695630 <44780914+589695630@users.noreply.github.com>
2019-04-09 10:29:40 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,31 @@
-package org.pltw.example.sfduedate;
+package statefarm.collegeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.gesture.Gesture;
 import android.view.View;
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity  {
+
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GestureListener gestureDetector1 = new GestureListener();
+
+        ;
+
     }
-    //This method would create and constrain groups when the button is clocked
-    //public void onFloatingActionButtonClick{View view}{
-        //group.create
-    //}
+
+
+    }
+
+
 }
